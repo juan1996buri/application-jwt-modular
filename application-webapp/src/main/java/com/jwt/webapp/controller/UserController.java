ea7eb0af6e5494b6e6c4ff2ad65ad5e36359489f
juan1996buri <juan98buri@gmail.com>
2022-08-22 11:39:06 -0500

    add username

--- application-webapp/src/main/java/com/jwt/webapp/controller/UserController.java
+++ application-webapp/src/main/java/com/jwt/webapp/controller/UserController.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,5 +70,18 @@ public class UserController {
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+	}
+
+	@GetMapping(value = "/username")
+	public ResponseEntity<Object> currentUserName() {
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			final String currentPrincipalName = authentication.getName();
+			System.out.println("Authentication: " + authentication);
+			System.out.println("Principal: " + authentication.getPrincipal());
+			return new ResponseEntity<>(new ApiResponseDTO<>(true, currentPrincipalName), HttpStatus.OK);
+		}
+
+		return new ResponseEntity<>(new ApiResponseDTO<>(true, false), HttpStatus.NOT_FOUND);
 	}
 }
